b96534f96bf9b0e1d43a0174328f59bf060d0d90
Hao <haosheng@stanford.edu>
2020-04-18 18:11:49 -0700

    update launchpad.py

--- launchpad.py
+++ launchpad.py
@@ -4,24 +4,49 @@ from config import Config
 import uuid
 import fire
 
-def launch(config="config.yaml"):
-    config = Config(config)
+
+def main(config="config.yaml",
+         run="compile"):
     
-    meta, hp = config.meta, config.hp
-    for _ in range(meta.repeat):
+    _config = Config(config)
+    meta, hp = _config.meta, _config.hp
+
+    for i in range(meta.repeat):
         if meta.mode == "grid":
             config_list = list(ParameterGrid(hp))
         elif meta.mode == "random":
             config_list = list(ParameterSampler(hp, meta.sample))
-            
-        for idx, c in enumerate(config_list):
-            c['exp_name'] = exp_name = uuid.uuid4().hex
-            exec_line = f"python {meta.script} train" + " ".join([f"--{k} {v}" for k, v in c.items()])
 
-            with open(f'{meta.sandbox}/{exp_name}.sh', 'w') as outfile:
-                outfile.write("#!/bin/bash\n")
-                outfile.write(f"rm -rf {meta.sandbox}/{exp_name}\n")
-                outfile.write(f"rm -rf {meta.sandbox}/tb/{exp_name}\n")
-                outfile.write(exec_line)
-                
-            with open(f'{meta.sandbox}/{exp_name}.sh', 'w') as outfile:
+        for idx, c in enumerate(config_list):
+            if "key" in meta:
+                key_config = meta.key
+                c['exp_name'] = exp_name = "_".join([str(c[k]) for k in key_config]) + f"_round_{i}"
+            else:
+                c['exp_name'] = exp_name = uuid.uuid4().hex
+            exec_line = f"{meta.script} " + " ".join([f"--{k} {v}" for k, v in c.items()])
+
+            if run == "compile":
+                print(exec_line)
+            elif run == "shell":
+                print(exec_line)
+                check_call(exec_line, shell=True)
+            elif run == "sbatch":
+                with open("sbatch_template.sh") as f:
+                    template = f.read()
+
+                    template = template.replace("@GPUS", f"{meta.gpus}")
+                    template = template.replace("@EXCLUDE", f"{meta.exclude}")
+                    template = template.replace("@LOG", f"{meta.logpath}")
+                    template = template.replace("@NAME", f"{exp_name}")
+                    template = template.replace("@COMMAND", f"{exec_line}")
+
+                    sbatch_filepath = os.path.join(meta.sandbox, f"{exp_name}.sh")
+                    with open(sbatch_filepath, 'w') as f:
+                        f.write(template)
+
+                    check_call(f"sbatch {sbatch_filepath}", shell=True)
+                    print(exec_line)
+                    #os.remove(sbatch_filepath)
+
+if __name__ == "__main__":
+    fire.Fire(main)
