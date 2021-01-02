6e990495a544ed0dd3b389c29ee7fe95b23ad59b
Hao <haosheng@stanford.edu>
2021-01-01 22:37:57 -0800

    make config an iterator

--- launchpad/launchpad.py
+++ launchpad/launchpad.py
@@ -1,5 +1,4 @@
 import yaml
-from sklearn.model_selection import ParameterGrid, ParameterSampler
 from subprocess import check_call, check_output
 import uuid
 import fire
@@ -25,49 +24,39 @@ def run(config="config.yaml",
         config = os.path.join(config, "config.yaml")
     col, _ = shutil.get_terminal_size()
     _config = Config(config)
-    meta, hp, sbatch = _config.meta, _config.hp, _config.sbatch
-    jobs = []
-    for i in range(meta.repeat):
-        if meta.mode == "grid":
-            config_list = list(ParameterGrid(hp))
-        elif meta.mode == "random":
-            config_list = list(ParameterSampler(hp, meta.sample))
-        for idx, c in enumerate(config_list):
-            c['round'] = i 
-            _config.hp = Args(c)
 
-            job = Job(_config)
-            job.compile()
-            state = job.get_state()
-            jobs.append(job)
-                        
-            # Check state
-            if state == "Finish":
-                if 'override' in meta and meta['override']:
-                    logger.warning(
-                        f"Override existing experiment [{job._exp_name}].")
-                else:
-                    logger.warning(f"Skip existing experiment [{job._exp_name}].")
-            elif state == "Running":
+    for idx, c in enumerate(_config):
+        job = Job(c)
+        job.compile()
+        state = job.get_state()
+                    
+        # Check state
+        if state == "Finish":
+            if 'override' in meta and meta['override']:
                 logger.warning(
-                    f"Skip experiment [{job._exp_name}], which is already running.")
-            else: 
-                if state == "Compiled":
-                    logger.info(f"Recompiled experiment [{job._exp_name}].")
+                    f"Override existing experiment [{job._exp_name}].")
+            else:
+                logger.warning(f"Skip existing experiment [{job._exp_name}].")
+        elif state == "Running":
+            logger.warning(
+                f"Skip experiment [{job._exp_name}], which is already running.")
+        else: 
+            if state == "Compiled":
+                logger.info(f"Recompiled experiment [{job._exp_name}].")
 
-                # Execution 
-                if run == "shell":
-                    job.shell()
-                elif run == "sbatch":
-                    job.sbatch()
+            # Execution 
+            if run == "shell":
+                job.shell()
+            elif run == "sbatch":
+                job.sbatch()
 
-            print("-" * col)
-            print(f"Experiment No.{idx+1} -- [{job._exp_name}]:\n{job._exec_line}")
-            print(f"Current State: {colorful_state(state)}") 
-            if state == "Running":
-                print(f"Slurm job ID: {job._id}")
-                print("Latest metrics: ")
-                print(f"{job.get_metrics()}")
+        print("-" * col)
+        print(f"Experiment No.{idx+1} -- [{job._exp_name}]:\n{job._exec_line}")
+        print(f"Current State: {colorful_state(state)}") 
+        if state == "Running":
+            print(f"Slurm job ID: {job._id}")
+            print("Latest metrics: ")
+            print(f"{job.get_metrics()}")
 
 def main():
     fire.Fire(run)
--- test/test_launchpad.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import tempfile 
-from launchpad import run
-
-
-class UnitTests(unittest.TestCase):
-    def test_empty_config(self):
-        path = 
-        run(
