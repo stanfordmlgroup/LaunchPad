8b904fca58c5bae52da4a634c01333d6450b2e52
Hao <haosheng@stanford.edu>
2021-01-01 22:37:57 -0800

    make config an iterator

--- launchpad/util/config.py
+++ launchpad/util/config.py
@@ -1,5 +1,7 @@
 import yaml
 import os
+import copy
+from sklearn.model_selection import ParameterGrid, ParameterSampler
 
 
 class Args(dict):
@@ -13,7 +15,8 @@ class Args(dict):
         raise AttributeError("No such attribute: " + name)
 
     def __setattr__(self, name, value):
-        self[name] = value
+        self.__dict__[name] = value
+        #self[name] = value
 
     def __delattr__(self, name):
         if name in self:
@@ -32,14 +35,41 @@ class Config:
         self.meta['config_path'] = path
 
         if "sandbox" not in self.meta:
-            self.meta['sandbox'] = "~/.launchpad"
+            self.meta.sandbox = "~/.launchpad"
         self.meta.sandbox = os.path.abspath(os.path.expanduser(self.meta.sandbox))
-        
-        self.meta['log_dir'] = os.path.join(self.meta.sandbox, "log")
-        self.meta['sbatch_dir'] = os.path.join(self.meta.sandbox, "sbatch")
+
+        self.meta.log_dir = os.path.join(self.meta.sandbox, "log")
+        self.meta.sbatch_dir = os.path.join(self.meta.sandbox, "sbatch")
         os.makedirs(self.meta.log_dir, exist_ok=True) 
         os.makedirs(self.meta.sbatch_dir, exist_ok=True) 
 
-
         self.hp = Args(_config['hp'])
         self.sbatch = Args(_config['sbatch'])
+    
+    
+    def __iter__(self):
+        self.round = 0
+        self._hp = copy.deepcopy(self.hp)
+        self.params = iter(self._get_params_grid())
+        return self
+
+    def __next__(self):
+        try:
+            param = next(self.params)
+            param['round'] = self.round
+            self.hp = Args(param)
+            return self
+        except StopIteration:
+            self.round += 1
+            if self.round >= self.meta.repeat:
+                self.hp = self._hp
+                raise StopIteration
+            self.params = iter(self._get_params_grid())
+            return self.__next__()
+
+    def _get_params_grid(self):
+        if self.meta.mode == "grid":
+            return ParameterGrid(self._hp)
+        elif self.meta.mode == "random":
+            return ParameterSampler(self._hp, self.meta.sample)
+
